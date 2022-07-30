a4e5cf100dd557c2f0bd4f385fa796f59a79675c
Diego Belo <diego.belo955@gmail.com>
2022-07-30 19:43:58 -0300

    Apenas mobile, página home

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 
     const navUL = document.getElementById('nav-ul');
 
+    const principal = document.getElementById('apresentation');
+
     hamburguer.addEventListener('click', () => {
-        navUL.classList.toggle('show')
+        navUL.classList.toggle('show'),
+        principal.classList.add('teste')
     });
 
 
